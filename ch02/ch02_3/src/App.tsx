3645dbaf051e72b00dee78be36ce437538d1a965
hjcha <hjcha@markany.com>
2024-07-04 17:00:07 +0900

    feat: "클래스 컴포넌트 테스트"

--- ch02/ch02_3/src/App.tsx
+++ ch02/ch02_3/src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component, ReactNode } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    const isLoading = true
+    if (isLoading) {
+      return <div>Loading...</div>
+    }
+
+    return (
+      <ul>
+        <li>
+          <a href="http://www.google.com">
+            <p>go to Google</p>
+          </a>
+        </li>
+      </ul>
+    )
+  }
 }
-
-export default App;
