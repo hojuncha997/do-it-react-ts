a87923c040ad3c6cc9eb760b40f2ddce31607651
hjcha <hjcha@markany.com>
2024-07-05 13:59:38 +0900

    feat: 리액트 프레임워크의 이벤트 속성

--- ch02/ch02_5/src/pages/ReactOnClick.tsx
+++ ch02/ch02_5/src/pages/ReactOnClick.tsx
@@ -1,3 +1,15 @@
+import type { SyntheticEvent } from 'react'
+
 export default function ReactOnClick() {
-  return <div>CopyMe</div>
+  const onClick = (e: SyntheticEvent) => {
+    const { isTrusted, target, bubbles } = e
+    console.log('mouse click occurs on <button>', isTrusted, target, bubbles)
+  }
+
+  return (
+    <div>
+      <p>ReactOnClick</p>
+      <button onClick={onClick}>Click Me</button>
+    </div>
+  )
 }
