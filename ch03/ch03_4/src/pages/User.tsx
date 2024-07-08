04a987639257f98b0dd21526cd3157513d676285
hojuncha997 <hojun.cha997@gmail.com>
2024-07-09 00:17:20 +0900

    learn: User컴포넌트 만들기

--- ch03/ch03_4/src/pages/User.tsx
+++ ch03/ch03_4/src/pages/User.tsx
@@ -1,3 +1,27 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+import type {FC} from 'react'
+import {DivProps, Div, Avatar} from '../components'
+import * as D from '../data'
+
+
+export type UserProps = DivProps & {
+    user: D.IUser
 }
+
+const User: FC<UserProps> = ({user, ...props}) => {
+  const {name, email, jobTitle, avatar} = user
+
+  return (
+    <Div {...props}> 
+      <div className="flex p-2">
+        <Avatar src={avatar} size="2rem" />
+        <div className='ml-2'>
+          <p className="font-bold">{name}</p>
+          <p className="text-gray-500 line-clamp-1">{jobTitle}</p>
+          <p className='text-blue-500'>{email}</p>
+        </div>
+      </div>
+    </Div>
+  )
+}
+
+export default User
--- ch03/ch03_4/src/pages/UserContainer.tsx
+++ ch03/ch03_4/src/pages/UserContainer.tsx
@@ -1,3 +1,26 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+import {Title} from '../components'
+import * as D from '../data'
+import User from './User'
+
+
+export default function UserContainer() {
+  const children = D.makeArray(10)
+    .map(D.makeRandomUser)
+    .map(user => (
+      <User key={user.uuid} 
+      user={user} 
+      className='m-2 text-xs border-2 border-blue-300 rounded-lg'
+      minWidth='15rem'
+      width='20rem'
+      />
+    ))
+
+  return (
+    <section className='mt-4'>
+      <Title>UserContainer</Title>
+      <div className='flex flex-wrap item-center justify-center p-4 mt-4'>
+        {children}
+      </div>
+    </section>
+  )
 }
