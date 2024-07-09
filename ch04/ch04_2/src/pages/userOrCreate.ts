2a4963defb15ec1648fd308092672d9994c88cc5
hjcha <hjcha@markany.com>
2024-07-09 16:43:13 +0900

    learn: 설명 추가 및 코드 변경

--- ch04/ch04_2/src/pages/userOrCreate.ts
+++ ch04/ch04_2/src/pages/userOrCreate.ts
@@ -1,7 +1,9 @@
 const cache: Record<string, any> = {}
 
 export const useOrCreate = <T>(key: string, callback: () => T): T => {
-  if (!cache[key]) cache[key] = callback()
+  if (!cache[key]) {
+    return (cache[key] = callback())
+  }
   return cache[key] as T
 }
 
