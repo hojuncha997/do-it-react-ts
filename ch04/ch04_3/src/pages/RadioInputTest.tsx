aeae5b464ea4b4391f12de6872e4627f430bafc8
hjcha <hjcha@markany.com>
2024-07-11 10:59:57 +0900

    learn: 라디오 버튼 실습

--- ch04/ch04_3/src/pages/RadioInputTest.tsx
+++ ch04/ch04_3/src/pages/RadioInputTest.tsx
@@ -1,8 +1,45 @@
-export default function CopyMe() {
+import { useState, useMemo, useCallback, ChangeEvent } from "react";
+import { Title, Subtitle } from "../components";
+import * as D from "../data";
+
+export default function RadioInputTest() {
+  const jobTitles = useMemo(() => D.makeArray(4).map(D.randomJobTitle), []);
+
+  const [selectedJobTitle, setSelectedJotTitle] = useState<string>(
+    jobTitles[0]
+  );
+
+  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setSelectedJotTitle((notUsed) => e.target.value);
+  }, []);
+
+  const radioInputs = useMemo(
+    () =>
+      jobTitles.map((value, index) => (
+        <label key={index} className="flex justify-start cursor-pointer label">
+          <input
+            type="radio"
+            name="jobs"
+            className="mr-4 radio radio-primary"
+            checked={value === selectedJobTitle}
+            defaultValue={value}
+          />
+          <span className="label-text">{value}</span>
+        </label>
+      )),
+    [jobTitles, selectedJobTitle, onChange]
+  );
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">Copy Me</h2>
-      <div className="mt-4"></div>
+      <Title>RadioInputTest</Title>
+      <div className="flex flex-col items-center justify-center m-4">
+        <Subtitle>What is your job</Subtitle>
+        <Subtitle className="mt-4">Selected Job: {selectedJobTitle}</Subtitle>
+        <div className="flex justify-center p-4 mt-4">
+          <div className="flex flex-col mt-4">{radioInputs}</div>
+        </div>
+      </div>
     </section>
   );
 }
