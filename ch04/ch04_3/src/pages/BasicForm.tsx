ad7dea3fa9e54d926f6f8bdb557a376e09976016
hjcha <hjcha@markany.com>
2024-07-12 15:05:30 +0900

    learn: 객체 상태 + 배열상태 실습

--- ch04/ch04_3/src/pages/BasicForm.tsx
+++ ch04/ch04_3/src/pages/BasicForm.tsx
@@ -19,10 +19,44 @@ export default function BasicForm() {
     [name, email]
   );
 
+  const onChangeName = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setName((notUsed) => e.target.value);
+  }, []);
+
+  const onChangeEmail = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setEmail((notUsed) => e.target.value);
+  }, []);
+
+  // prettier-ignore
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">Copy Me</h2>
-      <div className="mt-4"></div>
+      <Title>BasicForm</Title>
+      <div className="flex justify-center mt-4">
+        <form onSubmit={onSubmit} >
+          
+          <div className="form-control">
+            <label className="label" htmlFor="name">
+              <span className="label-text">Username</span>
+            </label>
+            <input value={name} onChange={onChangeName} id="name" type="text"
+              placeholder="enter your name" className="input input-primary"/>
+          </div>
+
+          <div className="form-control">
+            <label className="label" htmlFor="email">
+              <span className="label-text">email</span>
+            </label>
+            <input value={email} onChange={onChangeEmail} id="email" type="email"
+              placeholder="enter your email" className="input input-primary"/>
+          </div>
+
+          <div className="flex justify-center mt-4">
+            <input type="submit" value="Submit" className="w-1/2 btn btn-sm btn-primary" />
+            <input type="button" defaultValue="Cancel" className="w-1/2 ml-4 btn btn-sm" />
+          </div>
+          
+        </form>
+      </div>
     </section>
   );
 }
