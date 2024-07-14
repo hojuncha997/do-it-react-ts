a2ca40308cd7f38e9c24ecf95effdd9d939b4aaf
hojuncha997 <hojun.cha997@gmail.com>
2024-07-14 17:04:51 +0900

    learn: useEffect, useLayoutEffect: 클래스 컴포넌트에서의 상태 구현

--- ch04/ch04_4/tailwind.config.js
+++ ch04/ch04_4/tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 }
